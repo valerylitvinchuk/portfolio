3783238b95e755e6e7a2d084003607b529c9130c
valerylitvinchuk <valery.litvinchuk@gmail.com>
2015-12-20 16:51:23 +0300

    более-менее всё работает

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,119 @@
-$(document).ready(function($) {
-	console.log('Файл JS подключен успешно');
-});
+// Модуль добавления проекта
+	var addProject = (function (){
+
+// Инициализация проекта
+	var init = function(){
+		_setUpListners();
+	};
+
+// Получаем название файла из пути
+	var _getNameFromPath = function (path) {
+	return path.replace(/\\/g, '/').replace(/.*\//, '');
+	};
+
+// Прослушка событий
+	var _setUpListners = function (){
+		$('.add-project-button').on('click', _showModal); // открыть модальное окно
+		$('.modal-form').on('submit', _addProject); // добавление проекта
+		$('#fileupload').on('change', _changefileUpload);
+	};
+
+// Изменили файл аплоад (добавили файл в файлаплоад)
+	var _changefileUpload = function (){
+	var input = $(this), // инпут type="file"
+			name = _getNameFromPath(input.val()); // имя загруженного файла
+
+	$('#filename')
+		.val(name) // 
+		.trigger('hideTooltip')
+		.removeClass('has-error'); 
+	};
+
+	var _showModal = function (ev) {
+		ev.preventDefault();
+		var divPopup = $('.pop-up'),
+				form = divPopup.find('.modal-form');
+
+		divPopup.bPopup({
+	    speed: 650,
+      transition: 'slideDown',
+      modalColor: '#727070',
+      onClose: function () {
+      	// form.find('.server-mes').text('').hide();
+      	form.trigger('reset');
+      }
+    });
+	};
+
+	// var _myFunction = function (ev) {
+
+	// 	ev.preventDefault();
+
+	// 	var form = $(this);
+
+	// 	if (true) {
+	// 		$('.error-mes').hide();
+	// 		$('.success-mes').show();
+	// 		console.log("Проходит");
+	// 	} else {
+	// 			$('.success-mes').hide();
+	// 			$('.error-mes').show();
+	// 			console.log("Не проходит");
+	// 		};
+	// };
+
+	var _addProject = function (ev) {
+		console.log('Добавление проекта');
+	  ev.preventDefault();
+
+		var form = $(this),
+		    url = $(this).attr('action'),
+		    defObject = _ajaxForm(form, url);
+	
+		    if (defObject) {
+		    	console.log('!!!!!!!');
+		    // ...дальнейшие действия с ответом с сервера
+		    	defObject.done(function(ans) {
+	
+						var successBox = form.find('.success-mes'),
+								errorBox = form.find('.error-mes');
+	
+						if (ans.status === 'OK') {
+
+							errorBox.hide();
+							successBox.show();
+						} else {
+							successBox.hide();
+							errorBox.show();
+						};
+					})
+		    }
+  };
+
+	var _ajaxForm = function (form, url) {
+
+				console.log('Ajax form');
+
+	      if (!validation.validateForm(form)) return false;  // Возвращает false, если не проходит валидацию
+	      var data = form.serialize(); // собираем данные из формы в объект data
+
+	      console.log(data);
+
+	      return $.ajax({ // Возвращает Deferred Object
+	        type: 'POST',
+	        url: url,
+	        dataType : 'JSON',
+	        data: data
+	      }).fail( function(ans) {
+	        console.log('Проблемы в PHP');
+	        form.find('.error-mes').text('На сервере произошла ошибка').show();
+	      });
+	};
+
+	return {
+		init: init
+	};
+
+})();
+
+addProject.init();
--- app/js/validation.js
+++ app/js/validation.js
@@ -9,6 +9,7 @@ var validation = (function () {
 		// прослушка событий
 		$('form').on('keydown', '.has-error', _removeError);
 		$('form').on('reset', _clearForm);
+
 	};
 
 	var _removeError = function () {
@@ -19,8 +20,10 @@ var validation = (function () {
 		var form = $(this);
 		form.find('input, textarea').trigger('hideTooltip');
 		form.find('.has-error').removeClass('has-error');
+		$('.qtip').hide();
 	};
 
+	
 	// создание тултипа
 	var _createQtip = function (element, position) {
 		
@@ -28,12 +31,14 @@ var validation = (function () {
 		if (position === 'right') {
 			position = {
 				my: 'left center',
-				at: 'right center'
+				at: 'right center',
+				container: $('.pop-up')
 			}
 		} else {
 			position = {
 				my: 'right center',
 				at: 'left center',
+				container: $('.pop-up'),
 				adjust: {
 					method: 'shift none'
 				}
@@ -54,10 +59,10 @@ var validation = (function () {
 			},
 			position: position,
 			style: {
-				classes: 'qtip-red qtip-shadow qtip-rounded',
+				classes: 'qtip-mystyle',
 				tip: {
-					height: 10,
-					width: 16
+					height: 6,
+					width: 8
 				}
 			}
 		}).trigger('show');
@@ -67,22 +72,23 @@ var validation = (function () {
 	// Универсальная функция валидации
 	var validateForm = function (form) {
 
-		var elements = form.find('input, textarea').not('input[type="file"]', 'input[type="hidden"]'),
+		var elements = form.find('input, textarea').not('input[type="file"], input[type="hidden"], input[type="submit"]'),
 				valid = true;
-
-				console.log(elements);
 
 		$.each(elements, function(index, val){
 			var element = $(val),
 					val = element.val();
 					pos = element.attr('qtip-position');
 
+					console.log(val.length);
+
 			if (val.length === 0) {
 				element.addClass('has-error');
 				_createQtip (element, pos);
 				valid = false;
-			}
 
+			}
+			
 		});
 
 		return valid;
